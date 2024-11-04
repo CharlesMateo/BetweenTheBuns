f4a33a33ac75a2a60530fef7a2209b82f599881f
Moon Knight <Charlesmateo18@gmail.com>
2024-11-04 15:53:07 -0700

    Created an order class and a SAndwhich class

---
+++ src/main/java/com/pluralsight/Sandwhich.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Sandwhich {
+    protected int size; // 4, 8, or 12in
+    protected String breadType; // White, wheatt, rye, or a wrap
+    protected boolean toasted;
+
+}
