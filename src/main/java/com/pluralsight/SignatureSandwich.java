b1b2bea66083d3229a65bc132c639357456528a1
Moon Knight <Charlesmateo18@gmail.com>
2024-11-08 14:09:03 -0700

    Made some changes to Signature Sandwich class and Signature Type to make it much easier to manage sandwich configurations in one place, simplified the constructSignatureSandwich method, and made minor improvements in the getDetails method in Signature Sandwich

--- src/main/java/com/pluralsight/SignatureSandwich.java
+++ src/main/java/com/pluralsight/SignatureSandwich.java
@@ -1,156 +1,53 @@
 package com.pluralsight;
 
+import java.util.List;
+
 public class SignatureSandwich extends Sandwich {
-    private final SignatureType type;  // New field to store the type of signature sandwich
+    private final SignatureType type;
 
     public SignatureSandwich(SignatureType type) {
-        super(getDefaultSize(type), getDefaultBread(type));
+        super(type.getDefaultSize(), type.getDefaultBread());
         this.type = type;
         constructSignatureSandwich(type);
     }
 
-    // Getter method for the signature type
     public SignatureType getSignatureType() {
         return this.type;
     }
 
+    // Build the sandwich based on type details stored in the SignatureType enum
     private void constructSignatureSandwich(SignatureType type) {
-        switch (type) {
-            case BLT:
-                setSize(SandwichSize.EightInch);
-                setBread(BreadType.White);
-                addTopping(new PremiumTopping("Bacon", ToppingType.Bacon));
-                addTopping(new RegularTopping("Lettuce"));
-                addTopping(new RegularTopping("Tomato"));
-                addTopping(new RegularTopping("Mayo"));
-                setToasted(true);
-                break;
-
-            case ThePhilly:
-                setSize(SandwichSize.EightInch);
-                setBread(BreadType.White);
-                addTopping(new PremiumTopping("Steak", ToppingType.Meat));
-                addTopping(new PremiumTopping("Cheese", ToppingType.Cheese));
-                addTopping(new RegularTopping("Peppers"));
-                addTopping(new RegularTopping("Lettuce"));
-                addTopping(new RegularTopping("Tomato"));
-                addTopping(new RegularTopping("Mayo"));
-                setToasted(true);
-                break;
-
-            case WelcomeToTheClub:
-                setSize(SandwichSize.EightInch);
-                setBread(BreadType.White);
-                addTopping(new PremiumTopping("Honry Glazed Turkey", ToppingType.Meat));
-                addTopping(new PremiumTopping("Swiss Cheese", ToppingType.Cheese));
-                addTopping(new RegularTopping("Lettuce"));
-                addTopping(new RegularTopping("Tomato"));
-                addTopping(new RegularTopping("Mayo"));
-                setToasted(true);
-                break;
-
-            case TheCheesiest:
-                setSize(SandwichSize.TwelveInch);
-                setBread(BreadType.Rye);
-                addTopping(new PremiumTopping("Swiss Cheese", ToppingType.Cheese));
-                addTopping(new PremiumTopping("Monterey Jack Cheese", ToppingType.Cheese));
-                addTopping(new PremiumTopping("Mozzerella Cheese", ToppingType.Cheese));
-                addTopping(new RegularTopping("Butter"));
-                setToasted(true);
-                break;
-
-            case VegetariansArePeopleToo:
-                setSize(SandwichSize.TwelveInch);
-                setBread(BreadType.Wheat);
-                addTopping(new PremiumTopping("Lettuce", ToppingType.Vegetable));
-                addTopping(new PremiumTopping("Tomato", ToppingType.Vegetable));
-                addTopping(new PremiumTopping("Cucumber", ToppingType.Vegetable));
-                addTopping(new RegularTopping("Peppers"));
-                addTopping(new RegularTopping("Onions"));
-                addTopping(new PremiumTopping("Salt, Pepper, and Vinaigrette", ToppingType.OliveOilAndVinegar));
-                setToasted(false);
-                break;
-
-            default:
-                throw new IllegalArgumentException("Unknown signature sandwich" + type);
-        }
+        setSize(type.getDefaultSize());
+        setBread(type.getDefaultBread());
+        type.getToppings().forEach(this::addTopping);  // Add predefined toppings
+        setToasted(type.isToasted());
     }
 
     @Override
     public String getDetails() {
         StringBuilder details = new StringBuilder();
-
-        details.append(SignatureSandwich.getSignatureName(this.getSignatureType()))
+        details.append(getSignatureName(type))
                 .append(" (")
                 .append(getSize())
                 .append(" ")
                 .append(getBread())
                 .append(" Sandwich");
 
-        // Check if the sandwich is toasted
         if (isToasted()) {
             details.append(" - Toasted");
         }
-
         details.append(") with:\n");
 
-        // List all toppings
         for (Topping topping : getToppings()) {
             details.append("  - ").append(topping.getName()).append("\n");
         }
 
-        // Append the total calculated price
         details.append("Total Price: $").append(String.format("%.2f", calculatePrice()));
-
         return details.toString();
     }
 
-    // Private helper method to provide default size for each signature sandwich
-    private static SandwichSize getDefaultSize(SignatureType type) {
-        switch (type) {
-            case BLT:
-            case ThePhilly:
-            case WelcomeToTheClub:
-                return SandwichSize.EightInch;
-            case TheCheesiest:
-            case VegetariansArePeopleToo:
-                return SandwichSize.TwelveInch;
-            default:
-                throw new IllegalArgumentException("Unknown signature sandwich type: " + type);
-        }
-    }
-
-    // Private helper method to provide default bread type for each signature sandwich
-    private static BreadType getDefaultBread(SignatureType type) {
-        switch (type) {
-            case BLT:
-            case ThePhilly:
-            case WelcomeToTheClub:
-                return BreadType.White;
-            case TheCheesiest:
-                return BreadType.Rye;
-            case VegetariansArePeopleToo:
-                return BreadType.Wheat;
-            default:
-                throw new IllegalArgumentException("Unknown signature sandwich type: " + type);
-        }
-    }
-
-    // Helper method to return a user-friendly name for each signature sandwich
     public static String getSignatureName(SignatureType type) {
-        switch (type) {
-            case BLT:
-                return "BLT";
-            case ThePhilly:
-                return "The Philly";
-            case WelcomeToTheClub:
-                return "Welcome to the Club";
-            case TheCheesiest:
-                return "The Cheesiest";
-            case VegetariansArePeopleToo:
-                return "Vegetarians Are People Too";
-            default:
-                throw new IllegalArgumentException("Unknown signature sandwich type: " + type);
-        }
+        return type.getFriendlyName();
     }
 }
+
--- src/main/java/com/pluralsight/SignatureType.java
+++ src/main/java/com/pluralsight/SignatureType.java
@@ -1,20 +1,80 @@
 package com.pluralsight;
 
-public enum SignatureType {
-    BLT,
-    ThePhilly,
-    WelcomeToTheClub,
-    TheCheesiest,
-    VegetariansArePeopleToo;
+import java.util.Arrays;
+import java.util.List;
 
-    public String getName() {
-        switch (this) {
-            case BLT: return "BLT";
-            case ThePhilly: return "The Philly";
-            case WelcomeToTheClub: return "Welcome to the Club";
-            case TheCheesiest: return "The Cheesiest";
-            case VegetariansArePeopleToo: return "Vegetarians Are People Too";
-            default: throw new IllegalArgumentException("Unknown signature sandwich type: " + this);
-        }
+public enum SignatureType {
+    BLT("BLT", SandwichSize.EightInch, BreadType.White,
+            Arrays.asList(new PremiumTopping("Bacon", ToppingType.Bacon),
+                    new RegularTopping("Lettuce"),
+                    new RegularTopping("Tomato"),
+                    new RegularTopping("Mayo")),
+            true),
+
+    ThePhilly("The Philly", SandwichSize.EightInch, BreadType.White,
+            Arrays.asList(new PremiumTopping("Steak", ToppingType.Meat),
+                    new PremiumTopping("Cheese", ToppingType.Cheese),
+                    new RegularTopping("Peppers"),
+                    new RegularTopping("Lettuce"),
+                    new RegularTopping("Tomato"),
+                    new RegularTopping("Mayo")),
+            true),
+
+    WelcomeToTheClub("Welcome to the Club", SandwichSize.EightInch, BreadType.White,
+            Arrays.asList(new PremiumTopping("Honey Glazed Turkey", ToppingType.Meat),
+                    new PremiumTopping("Swiss Cheese", ToppingType.Cheese),
+                    new RegularTopping("Lettuce"),
+                    new RegularTopping("Tomato"),
+                    new RegularTopping("Mayo")),
+            true),
+
+    TheCheesiest("The Cheesiest", SandwichSize.TwelveInch, BreadType.Rye,
+            Arrays.asList(new PremiumTopping("Swiss Cheese", ToppingType.Cheese),
+                    new PremiumTopping("Monterey Jack Cheese", ToppingType.Cheese),
+                    new PremiumTopping("Mozzarella Cheese", ToppingType.Cheese),
+                    new RegularTopping("Butter")),
+            true),
+
+    VegetariansArePeopleToo("Vegetarians Are People Too", SandwichSize.TwelveInch, BreadType.Wheat,
+            Arrays.asList(new PremiumTopping("Lettuce", ToppingType.Vegetable),
+                    new PremiumTopping("Tomato", ToppingType.Vegetable),
+                    new PremiumTopping("Cucumber", ToppingType.Vegetable),
+                    new RegularTopping("Peppers"),
+                    new RegularTopping("Onions"),
+                    new PremiumTopping("Salt, Pepper, and Vinaigrette", ToppingType.OliveOilAndVinegar)),
+            false);
+
+    private final String friendlyName;
+    private final SandwichSize defaultSize;
+    private final BreadType defaultBread;
+    private final List<Topping> toppings;
+    private final boolean toasted;
+
+    SignatureType(String friendlyName, SandwichSize defaultSize, BreadType defaultBread, List<Topping> toppings, boolean toasted) {
+        this.friendlyName = friendlyName;
+        this.defaultSize = defaultSize;
+        this.defaultBread = defaultBread;
+        this.toppings = toppings;
+        this.toasted = toasted;
+    }
+
+    public String getFriendlyName() {
+        return friendlyName;
+    }
+
+    public SandwichSize getDefaultSize() {
+        return defaultSize;
+    }
+
+    public BreadType getDefaultBread() {
+        return defaultBread;
+    }
+
+    public List<Topping> getToppings() {
+        return toppings;
+    }
+
+    public boolean isToasted() {
+        return toasted;
     }
 }
